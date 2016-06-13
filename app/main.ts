e3ea85c990b6eda43eb4334d731a50b3ef28b472
Igor Ilic <ilicigor@outlook.com>
2016-06-14 00:43:21 +0200

    finished, mistakes

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,18 @@
+import { OpaqueToken, provide, Inject } from '@angular/core';
 import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
+import {AppComponent, stateFn, Action} from './app.component';
+import 'rxjs/Rx';
+import { Subject } from 'rxjs/Subject';
+
+// -- DI config
+// const initState = new OpaqueToken("initState");
+// const dispatcher = new OpaqueToken("dispatcher");
+// const state = new OpaqueToken("state");
+
+// const stateAndDispatcher = [
+//   provide(initState, {useValue: {todos: [], visibilityFilter: 'SHOW_ALL'}}),
+//   provide(dispatcher, {useValue: new Subject<Action>(null)}),
+//   provide(state, {useFactory: stateFn, deps: [new Inject(initState), new Inject(dispatcher)]})
+// ];
 
 bootstrap(AppComponent);
